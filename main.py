520f1357e41bc57515d278e6d6174d21d9b40881
Jack Devonshire <itunes.jd@hotmail.com>
2023-11-02 21:24:06 +0000

    Creating a proper main program loop

--- main.py
+++ main.py
@@ -37,47 +37,52 @@ def cooldown_timer():
     can_switch_cameras = True
 
 
-# Initialise all webcams
-webcams = []
-for webcamId in config["webcam_ids"]:
-    if webcamId == config["favourite_webcam_id"]:
-        camera = Camera("", webcamId, config["measurement_range"], config["favourite_webcam_weighting"])
-    else:
-        camera = Camera("", webcamId, config["measurement_range"], 1)
-
-    webcams.append(camera)
-
-last_best_webcam = webcams[0]
-current_best_webcam = webcams[0]
-
-# Now load virtual webcam and initialise main program loop
-with pyvirtualcam.Camera(width=config["virtual_cam_width"], height=config["virtual_cam_height"], fps=config["virtual_cam_fps"]) as virtual_cam:
-    while True:
-        best_detections = 0
-        best_frame = None
-
-        if can_switch_cameras:
-            # Select the best frame based on the webcam with the most positive detections for given measurement range
-            for webcam in webcams:
-                current_detections = webcam.update_model()
-
-                if current_detections > best_detections:
-                    best_detections = current_detections
-                    current_best_webcam = webcam
+def main():
+    # Initialise all webcams
+    webcams = []
+    for webcamId in config["webcam_ids"]:
+        if webcamId == config["favourite_webcam_id"]:
+            camera = Camera("", webcamId, config["measurement_range"], config["favourite_webcam_weighting"])
         else:
-            current_best_webcam.update_model()
+            camera = Camera("", webcamId, config["measurement_range"], 1)
 
-        # If switching between webcams, add a cooldown before we can switch again
-        if last_best_webcam != current_best_webcam:
-            threading.Thread(target=cooldown_timer).start()
+        webcams.append(camera)
 
-        best_frame = current_best_webcam.get_last_frame()
-        last_best_webcam = current_best_webcam
+    last_best_webcam = webcams[0]
+    current_best_webcam = webcams[0]
 
-        # Format best frame for virtual camera
-        virtual_frame = best_frame
-        virtual_frame = cv2.resize(virtual_frame, (config["virtual_cam_width"], config["virtual_cam_height"]))
-        virtual_frame = cv2.cvtColor(virtual_frame, cv2.COLOR_RGB2BGR)  # Convert frame colour for virtual webcam
+    # Now load virtual webcam and initialise main program loop
+    with pyvirtualcam.Camera(width=config["virtual_cam_width"], height=config["virtual_cam_height"],
+                             fps=config["virtual_cam_fps"]) as virtual_cam:
+        while True:
+            best_detections = 0
+            best_frame = None
 
-        # Send frame to virtual camera
-        virtual_cam.send(virtual_frame)
+            if can_switch_cameras:
+                # Select the best frame based on the webcam with the most positive detections for given measurement range
+                for webcam in webcams:
+                    current_detections = webcam.update_model()
+
+                    if current_detections > best_detections:
+                        best_detections = current_detections
+                        current_best_webcam = webcam
+            else:
+                current_best_webcam.update_model()
+
+            # If switching between webcams, add a cooldown before we can switch again
+            if last_best_webcam != current_best_webcam:
+                threading.Thread(target=cooldown_timer).start()
+
+            best_frame = current_best_webcam.get_last_frame()
+            last_best_webcam = current_best_webcam
+
+            # Format best frame for virtual camera
+            virtual_frame = best_frame
+            virtual_frame = cv2.resize(virtual_frame, (config["virtual_cam_width"], config["virtual_cam_height"]))
+            virtual_frame = cv2.cvtColor(virtual_frame, cv2.COLOR_RGB2BGR)  # Convert frame colour for virtual webcam
+
+            # Send frame to virtual camera
+            virtual_cam.send(virtual_frame)
+
+if __name__ == "__main__":
+    main()
