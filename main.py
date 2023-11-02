414d767889841a6a2d9f0434e7eca8a0626532ec
Jack Devonshire <itunes.jd@hotmail.com>
2023-11-02 20:57:09 +0000

    Adding switching cooldown
    Adding a camera switching cooldown in seconds, which means that whenever the camera is changed, it cannot be changed back before the cooldown timer has ran. This further helps to prevent flicking between cameras or unstable transitions.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import cv2
 import pyvirtualcam
 
@@ -7,6 +9,7 @@ from Camera import Camera
 
 webcamIds = [0, 1]  # Device ids of all webcams
 measurement_range = 25  # Amount of measurements to base camera switching decisions off of
+cooldown_seconds = 2  # For these seconds after switching, the camera will not switch again regardless of detections
 
 virtual_cam_width = 1080
 virtual_cam_height = 1080
@@ -14,28 +17,50 @@ virtual_cam_fps = 60
 
 ### Main Program ###
 
-# First initialise all webcams
+global can_switch_cameras
+can_switch_cameras = True
+
+# This function allows for a cooldown timer between switching cameras
+def cooldown_timer():
+    global can_switch_cameras
+
+    can_switch_cameras = False
+    time.sleep(cooldown_seconds)
+    can_switch_cameras = True
+
+
+# Initialise all webcams
 webcams = []
 for webcamId in webcamIds:
     camera = Camera("", webcamId, measurement_range)
     webcams.append(camera)
 
-best_webcam = webcams[0]
+last_best_webcam = webcams[0]
+current_best_webcam = webcams[0]
+
 # Now load virtual webcam and initialise main program loop
 with pyvirtualcam.Camera(width=virtual_cam_width, height=virtual_cam_height, fps=virtual_cam_fps) as virtual_cam:
     while True:
         best_detections = 0
         best_frame = None
 
-        # Select the best frame based on the webcam with the most positive detections for given measurement range
-        for webcam in webcams:
-            current_detections = webcam.update_model()
+        if can_switch_cameras:
+            # Select the best frame based on the webcam with the most positive detections for given measurement range
+            for webcam in webcams:
+                current_detections = webcam.update_model()
 
-            if current_detections > best_detections:
-                best_detections = current_detections
-                best_webcam = webcam
+                if current_detections > best_detections:
+                    best_detections = current_detections
+                    current_best_webcam = webcam
+        else:
+            current_best_webcam.update_model()
 
-        best_frame = best_webcam.get_last_frame()
+        # If switching between webcams, add a cooldown before we can switch again
+        if last_best_webcam != current_best_webcam:
+            threading.Thread(target=cooldown_timer).start()
+
+        best_frame = current_best_webcam.get_last_frame()
+        last_best_webcam = current_best_webcam
 
         # Format best frame for virtual camera
         virtual_frame = best_frame
